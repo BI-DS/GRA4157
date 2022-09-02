e86b753b8ad35f856a636921e059bd27418179f9
Vegard Vinje <vegard@simula.no>
2022-09-02 14:00:43 +0200

    updates

---
+++ lectures/01-python-summary/read_cmd_args.py
@@ -0,0 +1,5 @@
+import sys
+
+arguments = sys.argv[1:]
+
+print(arguments)
--- lectures/01-python-summary/test_function.py
+++ lectures/01-python-summary/test_function.py
@@ -1,12 +1,17 @@
-
+import pytest
 
 
 # define aboslute value
 def f(x):
-
-
-
-
-
+    if x < 0:
+        return x
+    else:
+        return x
+        
 
 # test absolute value
+@pytest.mark.parametrize("x, expected_value", [(-1,1), (2,2)])
+
+def test_absolute_value_function(x, expected_value):
+    assert f(x) == expected_value
+    
