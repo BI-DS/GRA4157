543cb781264dfc0323c47668d7127ae7573965e5
Vegard Vinje <vegard.vinje@hafslund.no>
2024-09-13 14:51:30 +0200

    updates

---
+++ lectures/02-python-summary-2/exercises/test_function.py
@@ -0,0 +1,13 @@
+
+
+def test_w(x,y):
+    # We assume x and y are coordinates
+    # Now can we find out inside this function whether they would make a w-plot?
+
+    # first line is straigt line going down
+    # second line is straight line upwards
+    # third line is straight line going down
+    # fourth line is straight line upwards
+    # all lines need to connect to next line
+
+    return
